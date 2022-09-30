dd296ce0538753abe4e75dea7ecfeda37b69fb81
nquirogac <nquirogac@unal.edu.co>
2022-09-30 18:42:17 -0500

    Division

--- Laboratorio1-Github/src/MathFunctions.java
+++ Laboratorio1-Github/src/MathFunctions.java
@@ -2,4 +2,7 @@ public class MathFunctions {
     public static int multiplyNumber(int a, int b) {
         return a * b;
     }
+    public static int divideNumber(int a, int b) {
+        return a / b;
+    }
 }
